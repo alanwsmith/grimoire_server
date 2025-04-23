8e9fa28dfac46437f2ff392b33677f3bba0c9acc
Alan Smith <commit@alanwsmith.com>
2025-04-22 21:48:01 -0400

    Added style and getting URL

--- browser_extensions/firefox/grimoire_server.js
+++ browser_extensions/firefox/grimoire_server.js
@@ -1,11 +1,27 @@
-const popoverEl = document.createElement('div')
-popoverEl.innerHTML = `this is it`
-popoverEl.setAttribute('popover', '')
-document.body.appendChild(popoverEl)
+const sheet = document.createElement("style")
+sheet.innerText = `
+  .grimoirePopover {
+    background-color: green;
+  }`
+document.head.appendChild(sheet)
+
+const popEl = document.createElement('div')
+popEl.classList.add("grimoirePopover")
+popEl.innerHTML = `
+<div>ping here2</div>
+<div>
+<input type="text" value="${window.location}" />
+</div>
+`
+popEl.setAttribute('popover', '')
+document.body.appendChild(popEl)
 
 window.addEventListener('keyup', (event) => {
-  if (event.key === "1") {
-    popoverEl.togglePopover()
+  const isOpen = popEl.matches(':popover-open')
+  if (event.key === "1" && isOpen === false) {
+    popEl.showPopover()
+  } else if (event.key === "2" && isOpen === true) {
+    popEl.hidePopover()
   }
 })
 
