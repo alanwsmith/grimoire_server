158b3101c7ca53b3a434ceb0527b278d41a7d760
Alan Smith <commit@alanwsmith.com>
2025-04-23 17:40:08 -0400

    setting up submit preventdefault catcher

--- browser_extensions/firefox/grimoire_server.js
+++ browser_extensions/firefox/grimoire_server.js
@@ -9,8 +9,15 @@ function addStylesheet() {
   .grimoirePopover {
     width: min(80ch, 100% - 3rem);
     background-color: green;
+    & form {
+      display: grid;
+      grid-template-columns: 4rem 1fr;
+    }
     & input[type=text] {
       width: min(70ch, 100% - 5rem);
+    }
+    & input[type=submit] {
+      width: 10ch;
     }
     & h2 {
       margin: 0;
@@ -24,19 +31,6 @@ function addStylesheet() {
   document.head.appendChild(sheet)
 }
 
-function updateStorage() {
-  const url = window.location
-  const stateToStore = {}
-  textInputs.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    stateToStore[key] = el.value
-  })
-  textAreas.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    stateToStore[key] = el.innerHTML
-  })
-  localStorage.setItem(url, JSON.stringify(stateToStore))
-}
 
 function getValues() {
   const url = window.location
@@ -58,7 +52,7 @@ function populateValues() {
   })
   textAreas.forEach(key => {
     const el = document.querySelector(`#pop-${key}`)
-    el.innerHTML = captureState[key]
+    el.value = captureState[key]
   })
 }
 
@@ -73,6 +67,21 @@ function populateValues() {
 //   return "TODO: get the selection"
 // }
 
+function updateStorage() {
+  console.log(".")
+  const url = window.location
+  const stateToStore = {}
+  textInputs.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    stateToStore[key] = el.value
+  })
+  textAreas.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    stateToStore[key] = el.value
+    console.log(el.value)
+  })
+  localStorage.setItem(url, JSON.stringify(stateToStore))
+}
 
 function addHandlers() {
   window.addEventListener('keyup', (event) => {
@@ -86,22 +95,44 @@ function addHandlers() {
   textInputs.forEach(input => {
     const inputEl = document.querySelector(`#pop-${input}`)
     inputEl.addEventListener('input', updateStorage)
+    inputEl.addEventListener('keydown', (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        return false;
+      }
+    })
   })
+  textAreas.forEach(area => {
+    const areaEl = document.querySelector(`#pop-${area}`)
+    areaEl.addEventListener('input', updateStorage)
+  })
+  const formEl = document.querySelector(
+    '.grimoirePopover > form'
+  );
+  formEl.addEventListener(
+    'submit', 
+    (event) => {
+      event.preventDefault();
+      console.log("asdf");
+      setTimeout(() => {
+        console.log("werwre");
+        formEl.submit();
+      }, 2000);
+    }
+  )
 }
+
 
 function addPopover() {
   popEl = document.createElement('div')
   popEl.classList.add("grimoirePopover")
   popEl.innerHTML = `
   <h2>Grimoire Capture Tool</h2>
-  <div>
+  <form>
     <label for="pop-title">Title</label>
     <input id="pop-title" type="text" />
-  </div>
-  <div>
     <label for="pop-url">URL</label>
     <input id="pop-url" type="text" />
-  </div>
   <!--
   <div>
     <label for="popBookmark">Bookmark</label>
@@ -120,19 +151,17 @@ function addPopover() {
     <input id="popVideo" type="radio" name="popType" value="video"/> 
   </div>
   -->
-  <div>
     <label for="pop-notes">Notes</label>
     <textarea id="pop-notes"></textarea>
-  </div>
-  <div>
     <label for="pop-tags">Tags</label>
     <input id="pop-tags" type="text" value="" />
-  </div>
+    <div></div>
+    <input type="submit" name="submit" value="submit" />
+  </form>
   `
   popEl.setAttribute('popover', '')
   document.body.appendChild(popEl)
 }
-
 
 addStylesheet()
 addPopover()
--- src/assets/payload.js
+++
@@ -1,2 +0,0 @@
-console.log("Loaded payload here");
-
