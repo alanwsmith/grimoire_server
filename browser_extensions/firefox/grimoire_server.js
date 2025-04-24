0560528b8e8675fa1cd076e87e070852c4a03a12
Alan Smith <commit@alanwsmith.com>
2025-04-23 21:31:33 -0400

    Getting basic request to server

--- browser_extensions/firefox/grimoire_server.js
+++ browser_extensions/firefox/grimoire_server.js
@@ -3,82 +3,6 @@ const textInputs = ['url', 'title', 'tags']
 const textAreas = ['notes']
 let popEl;
 
-function addStylesheet() {
-  const sheet = document.createElement("style")
-  sheet.innerText = `
-  .grimoirePopover {
-    width: min(80ch, 100% - 3rem);
-    background-color: green;
-    & form {
-      display: grid;
-      grid-template-columns: 4rem 1fr;
-    }
-    & input[type=text] {
-      width: min(70ch, 100% - 5rem);
-    }
-    & input[type=submit] {
-      width: 10ch;
-    }
-    & h2 {
-      margin: 0;
-    }
-    #pop-notes{
-      width: min(70ch, 100% - 5rem);
-      height: 8rem;
-    }
-  }
-  `
-  document.head.appendChild(sheet)
-}
-
-function getValues() {
-  const url = window.location
-  const storedState = localStorage.getItem(url)
-  if (storedState === null) {
-    captureState['url'] = url;
-    captureState['title'] = document.title;
-    captureState['tags'] = '';
-    captureState['notes'] = getSelection();
-  } else {
-    captureState = JSON.parse(storedState)
-  }
-}
-
-function getSelection() {
-  const selection = document.getSelection();
-  const selectedText = selection.toString();
-  return selectedText
-  //const notesEl = document.querySelector('#captureNotes');
-  // notesEl.innerText = selectedText;
-}
-
-function populateValues() {
-  textInputs.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    el.value = captureState[key]
-  })
-  textAreas.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    el.value = captureState[key]
-  })
-}
-
-function updateStorage() {
-  console.log(".")
-  const url = window.location
-  const stateToStore = {}
-  textInputs.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    stateToStore[key] = el.value
-  })
-  textAreas.forEach(key => {
-    const el = document.querySelector(`#pop-${key}`)
-    stateToStore[key] = el.value
-    console.log(el.value)
-  })
-  localStorage.setItem(url, JSON.stringify(stateToStore))
-}
-
 function addHandlers() {
   window.addEventListener('keyup', (event) => {
     const isOpen = popEl.matches(':popover-open')
@@ -102,16 +26,11 @@ function addHandlers() {
     const areaEl = document.querySelector(`#pop-${area}`)
     areaEl.addEventListener('input', updateStorage)
   })
-  const formEl = document.querySelector(
-    '.grimoirePopover > form'
+  const submitEl = document.querySelector(
+    '#submit-button'
   );
-  formEl.addEventListener(
-    'submit', 
-    (event) => {
-      event.preventDefault();
-      localStorage.removeItem(window.location);
-      formEl.submit();
-    }
+  submitEl.addEventListener(
+    'click', sendData
   )
 }
 
@@ -120,7 +39,7 @@ function addPopover() {
   popEl.classList.add("grimoirePopover")
   popEl.innerHTML = `
   <h2>Grimoire Capture Tool</h2>
-  <form>
+  <div class="inputs">
     <label for="pop-title">Title</label>
     <input id="pop-title" type="text" />
     <label for="pop-url">URL</label>
@@ -148,27 +67,109 @@ function addPopover() {
     <label for="pop-tags">Tags</label>
     <input id="pop-tags" type="text" value="" />
     <div></div>
-    <input type="submit" name="submit" value="submit" />
-  </form>
+    <button id="submit-button">Submit</button>
+  </div>
   `
   popEl.setAttribute('popover', '')
   document.body.appendChild(popEl)
 }
 
+function addStylesheet() {
+  const sheet = document.createElement("style")
+  sheet.innerText = `
+  .grimoirePopover {
+    width: min(80ch, 100% - 3rem);
+    background-color: green;
+    & .inputs {
+      display: grid;
+      grid-template-columns: 4rem 1fr;
+    }
+    & input[type=text] {
+      width: min(70ch, 100% - 5rem);
+    }
+    & #submit-button {
+      width: 10ch;
+    }
+    & h2 {
+      margin: 0;
+    }
+    #pop-notes{
+      width: min(70ch, 100% - 5rem);
+      height: 8rem;
+    }
+  }
+  `
+  document.head.appendChild(sheet)
+}
+
+function getSelection() {
+  const selection = document.getSelection();
+  const selectedText = selection.toString();
+  return selectedText
+}
+
+function getValues() {
+  const url = window.location
+  const storedState = localStorage.getItem(url)
+  if (storedState === null) {
+    captureState['url'] = url;
+    captureState['title'] = document.title;
+    captureState['tags'] = '';
+    captureState['notes'] = getSelection();
+  } else {
+    captureState = JSON.parse(storedState)
+  }
+}
+
+function populateValues() {
+  textInputs.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    el.value = captureState[key]
+  })
+  textAreas.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    el.value = captureState[key]
+  })
+}
+
+async function sendData() {
+  const postToUrl = "http://localhost:4545/api/make-note";
+  const bodyData = JSON.stringify(captureState);
+  try {
+    const response = await fetch(
+      postToUrl, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: bodyData,
+    });
+    console.log(response);
+    const result = await response.json();
+    // showOutput(result);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function updateStorage() {
+  console.log(".")
+  const url = window.location
+  const stateToStore = {}
+  textInputs.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    stateToStore[key] = el.value
+  })
+  textAreas.forEach(key => {
+    const el = document.querySelector(`#pop-${key}`)
+    stateToStore[key] = el.value
+    console.log(el.value)
+  })
+  localStorage.setItem(url, JSON.stringify(stateToStore))
+}
+
 addStylesheet()
 addPopover()
 addHandlers()
-
-
-
-// async function copySelection() {
-//   try {
-//     const selection = document.getSelection();
-//     const selectedText = selection.toString();
-//     const notesEl = document.querySelector('#captureNotes');
-//     notesEl.innerText = selectedText;
-//   } catch (err) {
-//     console.error("Could not copy selection to clipboard")
-//   }
-// }
 
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
-use axum::{Router, response::Html, routing::get};
-use axum_embed::{FallbackBehavior, ServeEmbed};
-use rust_embed::RustEmbed;
+use axum::{Router, extract, response, routing::get, routing::post};
+use serde::{Deserialize, Serialize};
 
-#[derive(RustEmbed, Clone)]
-#[folder = "src/assets"]
-struct Assets;
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct MakeNoteInput {
+    notes: String,
+    tags: String,
+    title: String,
+    url: String,
+}
+
+#[derive(Serialize)]
+struct MakeNoteResponse {
+    id: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(serve_home_page)).nest_service(
-        "/assets",
-        ServeEmbed::<Assets>::with_parameters(None, FallbackBehavior::NotFound, None),
-    );
+    let app = Router::new()
+        .route("/", get(serve_home_page))
+        .route("/api/make-note", post(handle_make_note));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4545").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn serve_home_page() -> Html<&'static str> {
-    let response = r#"<!DOCTYPE html>
-<html>
-    <body>
-        <h1>Grimoire Server</h1>
-        <p>
-            Used to accept incoming 
-            transmissions for the grimoire
-        </p>
-    </body>
-</html>"#;
-    Html(response)
+async fn serve_home_page() -> response::Html<&'static str> {
+    let response = include_str!("files/index.html");
+    response::Html(response)
+}
+
+async fn handle_make_note(
+    extract::Json(payload): extract::Json<MakeNoteInput>,
+) -> response::Json<MakeNoteResponse> {
+    dbg!(&payload);
+    let response = MakeNoteResponse {
+        id: "asdf".to_string(),
+    };
+    response::Json(response)
 }
